8a0385e7e562273914f14f83f70484bd5fc24de9
Marius2311 <72920791+Marius2311@users.noreply.github.com>
2023-03-31 11:59:14 +0200

    Adding Features for Grafana

--- migrations/V004__Prediction_Outputs.sql
+++ migrations/V004__Prediction_Outputs.sql
@@ -1,6 +1,9 @@
 CREATE TABLE IF NOT EXISTS "weekly_forecast" (
       "id" BIGINT NOT NULL,
       "product_id" BIGINT NOT NULL,
+      "product_price" NUMERIC(10,2) NOT NULL,
+      "product_category" TEXT NOT NULL,
+      "product_id_str" TEXT NOT NULL,
       "predicted_quantity" NUMERIC(10,2) NOT NULL,
       PRIMARY KEY ("id"),
       CONSTRAINT "fk_product" FOREIGN KEY ("product_id") REFERENCES "products" ("id") ON UPDATE NO ACTION ON DELETE RESTRICT
@@ -9,6 +12,7 @@ CREATE TABLE IF NOT EXISTS "weekly_forecast" (
 CREATE TABLE IF NOT EXISTS "daily_forecast" (
       "id" BIGINT NOT NULL,
       "product_id" BIGINT NOT NULL,
+      "date" TIMESTAMP NOT NULL,
       "year" INTEGER NOT NULL,
       "month" INTEGER NOT NULL,
       "day" INTEGER NOT NULL,
